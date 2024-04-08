bdedca6498a41f93180994410a3d34ed23c82bb1
moirachen1019 <peggy10191019@gmail.com>
2024-04-08 10:19:00 +0000

    install net-tools

--- install_elastic.sh
+++ install_elastic.sh
@@ -11,6 +11,7 @@ apt update
 apt install -y apt-transport-https openjdk-8-jre-headless
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/elastic-archive-keyring.gpg
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-6.x.list
+apt update && sudo apt install net-tools
 apt update && sudo apt install -y elasticsearch
 /bin/systemctl daemon-reload
 /bin/systemctl enable elasticsearch.service
